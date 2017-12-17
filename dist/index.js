cccd8f7535d4d7312f642d0395f859192e8f2536
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2017-12-17 12:36:14 +0100

    removed dist files

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import 'jquery';
-var di = require('node-di2');
-di.injectWithName(['$master'], function (master) {
-    master(module.filename, './master');
-})();
-
-//# sourceMappingURL=index.js.map
--- dist/server/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import 'jquery';
-var di = require("akala-core");
-di.injectWithName(['$master'], function (master) {
-    master(module.filename, './master');
-})();
-
-//# sourceMappingURL=index.js.map
